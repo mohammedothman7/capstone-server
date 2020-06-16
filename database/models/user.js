7a52bf40e6076eaf85fecc8b6b347a9e80dbb356
Mohammed Othman <mohammed.othman7@cix.csi.cuny.edu>
2020-06-16 11:05:29 -0400

    Added model for users table in database

---
+++ database/models/user.js
@@ -0,0 +1,12 @@
+const Sequelize = require('sequelize');
+const db = require('../db');
+
+const User = db.define('user', {
+  firstName: { type: Sequelize.STRING, allowNull: false },
+  lastName: { type: Sequelize.STRING, allowNull: false },
+  email: { type: Sequelize.STRING, allowNull: false },
+  username: { type: Sequelize.STRING, allowNull: false },
+  password: { type: Sequelize.STRING, allowNull: false },
+});
+
+module.exports = User;
