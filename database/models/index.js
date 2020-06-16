81131e62bcf5066a94e3e85dadf892932aad7437
Mohammed Othman <mohammed.othman7@cix.csi.cuny.edu>
2020-06-16 11:27:50 -0400

    Added User and Comments to barrel file and created relations between the two

--- database/models/index.js
+++ database/models/index.js
@@ -1,7 +1,11 @@
 // Here, we can prepare to register our models, set up associations between tables, and generate a barrel file for the models;
+
+const User = require('./user');
+const Comments = require('./comments');
+
+User.hasMany(Comments);
+Comments.belongsTo(User);
 /*
-const Campus = require("./campus");
-const Student = require("./student");
 
 Campus.hasMany(Student);
 
@@ -12,3 +16,8 @@ module.exports = {
   Student,
 };
 */
+
+module.exports = {
+  User,
+  Comments,
+};
