70322603aa497399ce38de7ec6291e9ec86382f4
Mohammed Othman <mohammed.othman7@cix.csi.cuny.edu>
2020-06-18 12:24:17 -0400

    Added functionality to search for games

--- routes/games.js
+++ routes/games.js
@@ -1,19 +1,19 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-const axios = require('axios');
-const { Games } = require('../database/models');
+const axios = require("axios");
+const { Games } = require("../database/models");
 
 //localhost:3000/games
 
 // Route to get gamess from database
-router.get('/', async (req, res, next) => {
+router.get("/", async (req, res, next) => {
   // try to get gamess object from api
   //console.log(req);
   const { page, page_size } = req.query;
   // const param = req.query.filter;
   console.log(req.query);
   axios
-    .get('https://api.rawg.io/api/games', {
+    .get("https://api.rawg.io/api/games", {
       params: {
         page,
         page_size,
@@ -35,6 +35,41 @@ router.get('/', async (req, res, next) => {
         //console.log(game);
       }
       //console.log(games);
+      res.status(200).json(allGamesArray);
+    })
+    .catch((error) => {
+      next(error);
+    });
+});
+
+// Route to search for games
+router.get("/search", async (req, res, next) => {
+  // Get search query from request
+  const { search } = req.query;
+
+  // Call API to search for games
+  axios
+    .get("https://api.rawg.io/api/games", {
+      params: {
+        search,
+      },
+    })
+    .then((response) => {
+      const games = response.data.results;
+      const allGamesArray = [];
+
+      // Iterrate through array to get data we need
+      for (let i = 0; i < games.length; i++) {
+        const game = {
+          id: games[i].id,
+          name: games[i].name,
+          background_image: games[i].background_image,
+          metacritic: games[i].metacritic,
+        };
+        allGamesArray.push(game);
+      }
+
+      // Send data back to frontend
       res.status(200).json(allGamesArray);
     })
     .catch((error) => {
