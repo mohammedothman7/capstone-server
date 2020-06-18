2c82cf983438a338c3effa4214a6becab935062b
Jan Markus Milan <jmarkusm@me.com>
2020-06-18 15:30:30 -0400

    Pulled comments

---
+++ database/models/comments.js
@@ -0,0 +1,10 @@
+const Sequelize = require("sequelize");
+const db = require("../db");
+
+const Comments = db.define("comments", {
+  gameId: { type: Sequelize.INTEGER, allowNull: false },
+  userId: { type: Sequelize.INTEGER, allowNull: true },
+  commentContent: { type: Sequelize.TEXT, allowNull: false },
+});
+
+module.exports = Comments;
