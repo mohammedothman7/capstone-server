7bccb914f7ddf8e3d8ef950363f2f98072c36620
Mohammed Othman <mohammed.othman7@cix.csi.cuny.edu>
2020-06-23 16:01:07 -0400

    Added env variable for PORT

--- app.js
+++ app.js
@@ -48,7 +48,10 @@ const configureApp = () => {
   app.use(helmet());
   app.use(logger("dev"));
   // handle request data:
-  app.use(cors());
+  app.use(cors(), {
+    withCredentials: true,
+    origin: "https://gamecord.netlify.app",
+  });
   app.use(express.json());
   app.use(express.urlencoded({ extended: false }));
   app.use(compression());
--- database/db.js
+++ database/db.js
@@ -11,19 +11,18 @@ console.log("Opening database connection");
 console.log(process.env.HEROKU_POSTGRESQL_PURPLE_URL);
 // This is our entry point, we instantiate the Sequelize instance accordingly;
 const db = new Sequelize(
-  process.env.HEROKU_POSTGRESQL_PURPLE_URL ||
-    `postgres://localhost:5432/${databaseName}`,
+  process.env.PORT || `postgres://localhost:5432/${databaseName}`,
   { logging: false }
 );
 
-const client = new Client({
-  connectionString: process.env.HEROKU_POSTGRESQL_PURPLE_URL,
-  ssl: {
-    rejectUnauthorized: false,
-  },
-});
+// const client = new Client({
+//   connectionString: process.env.HEROKU_POSTGRESQL_PURPLE_URL,
+//   ssl: {
+//     rejectUnauthorized: false,
+//   },
+// });
 
-client.connect();
+//client.connect();
 
 // Export our instance of Sequelize, which will be modified with models;
-module.exports = client;
+module.exports = db;
